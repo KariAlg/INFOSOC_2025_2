68c31511da6c15ab652fb8b763e5d1393240916d
Git-InE <briosl@usm.cl>
2025-10-31 15:42:12 -0300

    LOCURAAAA

--- config/usuarios/urls.py
+++ config/usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.panel_home, name="panel_home"),
@@ -11,33 +12,35 @@ urlpatterns = [
     path("crear/", views.crear_usuario, name="crear_usuario"),
     path("listar/", views.listar_usuarios, name="listar_usuarios"),
     path("editar/<str:rut>/", views.editar_usuario, name="editar_usuario"),
-
     path("modificar/", views.modificar_mi_perfil, name="modificar_mi_perfil"),
     path("borrar/<str:rut>/", views.borrar_usuario, name="borrar_usuario"),
 
-    path("password-reset/",
-        views.PasswordResetView.as_view(
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
             template_name="registration/password_reset_form.html"
         ),
-        name="password_reset"),
-
-    path("password-reset/done/",
-        views.PasswordResetDoneView.as_view(
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
             template_name="registration/password_reset_done.html"
         ),
-        name="password_reset_done"),
-
-    path("reset/<uidb64>/<token>/",
-        views.PasswordResetConfirmView.as_view(
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
             template_name="registration/password_reset_confirm.html"
         ),
-        name="password_reset_confirm"),
-
-    path("reset/done/",
-        views.PasswordResetCompleteView.as_view(
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
             template_name="registration/password_reset_complete.html"
         ),
-        name="password_reset_complete"),
-
-
+        name="password_reset_complete",
+    ),
 ]
